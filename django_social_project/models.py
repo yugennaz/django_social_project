e93b0d7b93718e8da8d88e0d8fa7cddc1b2992b9
Nazira17 <you@example.com>
2019-10-06 16:44:46 +0600

    Add models.py

---
+++ django_social_project/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    user = models.ForeignKey(
+        User, related_name='images',
+        on_delete=models.CASCADE
+    )
+    description = models.TextField(blank=True)
